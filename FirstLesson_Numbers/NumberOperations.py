2f804ec57bdd03b9404383e9ca809c8913cee292
aZacharakis <apostoliszax@hotmail.gr>
2023-09-23 14:37:13 +0300

    Initial Commit

---
+++ FirstLesson_Numbers/NumberOperations.py
@@ -0,0 +1,15 @@
+# Addition
+result = 5 + 3
+print(result)  # Output: 8
+
+# Subtraction
+result = 10 - 2
+print(result)  # Output: 8
+
+# Multiplication
+result = 4 * 6
+print(result)  # Output: 24
+
+# Division (floating-point)
+result = 9 / 2
+print(result)  # Output: 4.5
