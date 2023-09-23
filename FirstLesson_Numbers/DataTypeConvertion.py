9e3283b4c91a7d2e4db6d0c7f99df50f79cb9b01
aZacharakis <apostoliszax@hotmail.gr>
2023-09-23 15:37:03 +0300

    Math Functions

---
+++ FirstLesson_Numbers/DataTypeConvertion.py
@@ -0,0 +1,7 @@
+int_num = int(3.5)
+float_num = float(7)
+
+
+
+print(int_num)
+print(float_num)
---
+++ FirstLesson_Numbers/MathFunc.py
@@ -0,0 +1,17 @@
+import math
+
+# Square root
+result = math.sqrt(25)
+print(result)  # Output: 5.0
+
+result = math.sqrt(9)
+print(result)  # Output: 3.0
+
+
+# Trigonometric functions (e.g., sine)
+result = math.sin(math.pi / 2)
+print(result)  # Output: 1.0
+
+# Constants (e.g., pi)
+pi_value = math.pi
+print(pi_value)  # Output: 3.141592653589793
